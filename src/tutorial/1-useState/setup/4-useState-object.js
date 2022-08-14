954fd33e579cadafedc14c292d984230e7d0792f
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-14 17:15:36 +0200

    multiple State Values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,15 +6,20 @@ const UseStateObject = () => {
     age: 24,
     message: 'kaizoku ou ni ore wa naru'
   });
+
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('kaizoku ou ni ore wa naru');
+
   const changeMessage = () => {
-    setPerson({
-      ...person, message: 'hello monkey'
-    })
-  }
+    // setPerson({ ...person, message: 'hello monkey'})
+    setMessage('hello monkey');
+  };
+
   return <>
-    <h3>{person.name}</h3>
-    <h3>{person.age}</h3>
-    <h3>{person.message}</h3>
+    <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
     <button className='btn' onClick={changeMessage}>
       change message
     </button>
