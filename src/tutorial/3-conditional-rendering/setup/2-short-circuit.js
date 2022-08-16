fedba2ab288540ec2dfd556277cf6813597b0a1d
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-16 13:10:40 +0200

    Ternary Operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,17 +4,25 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
-  console.log(secondValue);
-  return <>
-    {/* <h1>{firstValue}</h1> */}
-    {/* <h1>value: {secondValue}</h1> */}
-    {/* {if(){console.log('hello world')}} */}
-    <h1>{text || 'john doe'}</h1>
-    {text && <h1>hello world</h1>}
-    {!text && <h1>hello world</h1>}
-  </>;
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
+
+  return (
+    <>
+      {/* <h1>{firstValue}</h1> */}
+      {/* <h1>value: {secondValue}</h1> */}
+      {/* {if(){console.log('hello world')}} */}
+      <h1>{text || 'john doe'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>toggle error</button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>there is no error</div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
